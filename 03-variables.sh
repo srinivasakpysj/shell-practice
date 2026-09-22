f2ddfbdd9725cae9fd8f146a5ccb7535fe83b9e3
srinivasakysj <srinivasakysj@gmail.com>
2026-09-22 06:13:21 +0530

    variables changed for testing changing in main code or not

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,8 +2,8 @@
 
 # No space before or after equal
 
-PERSON1=Narendra Modi
-PERSON2=YS Jagan Mohan Reddy
+PERSON1=NarendraModi
+PERSON2=YSJaganMohanReddy
 
 echo "$PERSON1:: Hi $PERSON2, How are you doing?"
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"

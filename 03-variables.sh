ea938081ccb753be64be70b02dec69bcb550d7b5
srinivasakysj <srinivasakysj@gmail.com>
2026-09-22 06:10:56 +0530

    variables missed

--- 03-variables.sh
+++ 03-variables.sh
@@ -8,4 +8,4 @@ PERSON2=Rahul
 echo "$PERSON1:: Hi $PERSON2, How are you doing?"
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: What are you learning recently?"
-echo "PERSON2:: I am Learning Shell Scripting $PERSON1"
+echo "$PERSON2:: I am Learning Shell Scripting $PERSON1"

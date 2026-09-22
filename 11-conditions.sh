409fc513e219be82279d2d1f4a08e9058bb1868d
srinivasakysj <srinivasakysj@gmail.com>
2026-09-22 13:40:29 +0530

    EVEN & ODD number condition

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo " please enter the number:"
+echo "please enter the number:"
 read NUMBER
 
-if [(($NUMBER % 2)) -eq 0 ]; then
-	echo "given number $NUMBER is EVEN"
+if [ $(($NUMBER % 2)) -eq 0 ]; then
+	echo "Given number $NUMBER is EVEN"
 else
 	echo "Given number $NUMBER is ODD"
 fi

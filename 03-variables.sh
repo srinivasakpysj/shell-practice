451a95a8093f10cf3ab49f1d038cac151a91ba52
srinivasakysj <srinivasakysj@gmail.com>
2026-09-22 06:09:14 +0530

    variables created

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# No space before or after equal
+
+PERSON1=sachin
+PERSON2=Rahul
+
+echo "$PERSON1:: Hi $PERSON2, How are you doing?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: What are you learning recently?"
+echo "PERSON2:: I am Learning Shell Scripting $PERSON1"
